abd433d6cc34ffa95d848d1e8a549fb7c1d481b3
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-11-24 07:49:59 +0000

    0.0.3

--- sh/apply.sh
+++ sh/apply.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp -v src/dialer/index.mjs ../Xray-core/transport/internet/browser_dialer/dialer.mjs
-echo -e '<!DOCTYPE html>\n<head>\n\t<meta http-equiv="200" charset="utf-8" />\n\t<meta content="true" name="HandheldFriendly" />\n\t<title>Browser Dialer</title>\n\t<script type="module">\n' > ../Xray-core/transport/internet/browser_dialer/dialer.html
+echo -e '<!DOCTYPE html>\n<head>\n\t<meta http-equiv="200" charset="utf-8" />\n\t<meta content="true" name="HandheldFriendly" />\n\t<title>Browser Dialer</title>\n\t<script type="module">' > ../Xray-core/transport/internet/browser_dialer/dialer.html
 cat src/client/index.mjs >> ../Xray-core/transport/internet/browser_dialer/dialer.html
 echo -e '\n\t</script>\n</head>\n<body>\n</body>' >> ../Xray-core/transport/internet/browser_dialer/dialer.html
+sed -i "s/\/index//g" "../Xray-core/transport/internet/browser_dialer/dialer.html"
 exit
